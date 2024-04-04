e5d1a6f7129073a48cd46eec234d8df6c969c43d
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-04-04 00:22:58 -0700

    staerter

---
+++ src/main/java/com/manisha/java/Application.java
@@ -0,0 +1,13 @@
+package com.manisha.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("hello world 2!");
+	}
+
+}
