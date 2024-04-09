78da4ff40ceef0f45047f7c3081f00b5b4d2f337
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-04-09 00:01:18 -0700

    exercise-10

---
+++ src/main/java/com/manisha/java/fundamental/exercises/Exercise10_SecondsAndMinutes.java
@@ -0,0 +1,47 @@
+package com.manisha.java.fundamental.exercises;
+
+import com.manisha.java.util.Print;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Exercise10_SecondsAndMinutes {
+public static String getDurationString(int seconds)
+{
+    int minutes = seconds/60;
+    int secondRemainder = seconds % 60;
+    int hour = minutes/60;
+    int minuteRemainder = minutes % 60;
+    String duration  = hour+"h "+minuteRemainder+"m "+secondRemainder+"s";
+//    if ( seconds > 0){
+//        Print.info("seconds : "+seconds,duration);
+//    }
+//    else {
+//        Print.info("Invalid value");
+//    }
+    return duration;
+}
+
+public static String getDurationString(int min, int sec){
+    Print.info("minutes : "+ min, "Seconds : "+ sec);
+    int totalSeconds = (min * 60 )+ sec;
+    return getDurationString(totalSeconds);
+}
+
+public static void main(String[] args) {
+    getDurationString(4586);
+    getDurationString(120, 500);
+
+//
+//    LocalDateTime date = LocalDateTime.now();
+//    ZonedDateTime zone = ZonedDateTime.now();
+//    Print.info(date, zone);
+//    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy   hh:mm:ss:SSSS");
+//    DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MM/dd/yyyy   HH:mm:ss:SSSSSSS");
+//    Print.info(date.format(formatter), date.format(formatter2));
+ }
+
+}
