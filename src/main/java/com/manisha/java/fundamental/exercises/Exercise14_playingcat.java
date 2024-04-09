3edfd952193eeca6a6f544f87ae15e9edf715f0f
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-04-09 15:18:02 -0700

    exercise-13,14

---
+++ src/main/java/com/manisha/java/fundamental/exercises/Exercise14_playingcat.java
@@ -0,0 +1,29 @@
+package com.manisha.java.fundamental.exercises;
+
+import com.manisha.java.util.Print;
+
+public class Exercise14_playingcat {
+public static boolean isCatPlaying(boolean summer,int temparature){
+
+    if(summer){
+       return  (temparature >= 25 && temparature <= 45)? true : false;
+    }
+    else {
+        return  (temparature >= 25 && temparature <= 35)? true : false;
+    }
+
+
+}
+
+public static void main(String[] args) {
+    Print.info(isCatPlaying(true, 30));
+    Print.info(isCatPlaying(true, 10));
+    Print.info(isCatPlaying(true, 40));
+
+    Print.info(isCatPlaying(false, 36));
+    Print.info(isCatPlaying(false, 35));
+    Print.info(isCatPlaying(false, 50));
+
+
+    }
+}
