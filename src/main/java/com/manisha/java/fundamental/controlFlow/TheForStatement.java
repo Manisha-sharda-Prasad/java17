228d002bbf33418e334b965dee9a51b8ee057072
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-04-10 19:40:44 -0700

    for loop exercise

--- src/main/java/com/manisha/java/fundamental/controlFlow/TheForStatement.java
+++ src/main/java/com/manisha/java/fundamental/controlFlow/TheForStatement.java
@@ -6,14 +6,25 @@ public class TheForStatement {
 
     public static double calcInterest(double amount, double intRate){
 
-        double interestAmount = (amount * (intRate /100));
-
-        for ( double rate = 2.0; rate <= 5.0; rate++){
-            Print.info( "% interest of " + amount+ " at "+ rate + "% is " + interestAmount );
-        }
-        return interestAmount;
+        return  (amount * (intRate /100));
+    }
+    public static double calcuInt(double amount, double intRate){
+        return (amount * (intRate /100));
     }
     public static void main(String[]args){
-        calcInterest(10000, 2);
+        //1.)-------
+        for ( double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmount = calcInterest(10000.0, rate);
+            Print.info(" 10,000  at interest of " + rate + "% = " + interestAmount);
+
+        }
+        //2.)-------
+         for (double i = 7.5; i <= 10; i += 0.25) {
+            double intAmount =  calcuInt(500.0 , i );
+//           if (intAmount > 8.5){
+//                break;
+//            }
+             Print.info( " $500  amount interest of " + i + "% =  $" + intAmount );
+        }
     }
 }
