d30be9feec8a42b44b3c2cfe0df71f4dfe24e51b
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-04-30 14:12:02 -0700

    basic change

--- src/main/java/com/manisha/java/fundamental/exercise/ifElse/Exercise1_HighScore.java
+++ src/main/java/com/manisha/java/fundamental/exercise/ifElse/Exercise1_HighScore.java
@@ -6,34 +6,37 @@ public class Exercise1_HighScore {
 
 //Methods//if/else---------------------------------------------------------------------------------------
 
-static  void displayHighScorePosition( String playerName, int score){
-    int position = calcHighScorePosition(score);
-    Print.info(playerName + " managed to get into position " + position + " on the high score list");
-}
-
-static int calcHighScorePosition(int score){
+    //1---------------------
+    static int calcHighScorePosition(int score){
     int result = 0;
-     if (score >= 1000){
+     if (score >= 1500 && score > 1000){
          result = 1;
-     } else if (score >= 500 ) {
+     } else if (score >= 500 && score <= 1000) {
          result = 2;
-     } else if (score >= 100 ){
+     } else if (score >= 100){
          result = 3;
      }else{
          result = 4;
      }
          return result;
-}
+    }
+    //2---------------------
+    static  void displayHighScorePosition( String playerName, int score){
+        int position = calcHighScorePosition(score);
+        Print.info(playerName + " managed to get into position " + position + " on the high score list");
+    }
+    //3----------------------
+    static  void checkingScores(){
+     displayHighScorePosition("Tim" ,1500 );
+     displayHighScorePosition("Lek" ,1000 );
+     displayHighScorePosition("Manu" ,400 );
 
- static  void checkingScores(){
+    }
 
-    displayHighScorePosition("Tim" ,1500 );
-    displayHighScorePosition("Lek" ,500 );
-}
-
-   // ======================MAIN========================
+// ======================MAIN========================
     public static  void main(String[]args){
         checkingScores();
+
     }
 
 }
