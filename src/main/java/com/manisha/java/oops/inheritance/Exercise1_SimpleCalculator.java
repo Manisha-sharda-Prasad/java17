4e3091666ed778c600fb252c5871af325f3332a4
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-05-05 13:01:06 -0700

    setter getter

--- src/main/java/com/manisha/java/oops/inheritance/Exercise1_SimpleCalculator.java
+++ src/main/java/com/manisha/java/oops/inheritance/Exercise1_SimpleCalculator.java
@@ -4,7 +4,44 @@ public class Exercise1_SimpleCalculator {
     private double firstNumber;
     private double secondNumber;
 
+    private double additionalResult;
+    private double subtractionResult;
+    private double multiplicationResult;
+    private double divisionResult;
 
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+//set-get first num second num------->
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+//get- other methods------->
+    public double getAdditionalResult() {
+        return additionalResult;
+    }
+
+    public double getSubtractionResult() {
+        return subtractionResult;
+    }
+
+    public double getMultiplicationResult() {
+        return multiplicationResult;
+    }
+
+    public double getDivisionResult() {
+        return divisionResult;
+    }
 
 
 }
