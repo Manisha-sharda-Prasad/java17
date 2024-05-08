d208264a201410c6ee841599004c25594bd22196
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-05-08 15:07:59 -0700

    Strings--indexOf, lastIndexOf,equalsIgnoreCase, equals, starts-endsWith, contains

---
+++ src/main/java/com/manisha/java/oops/Inheritance/Strings.java
@@ -0,0 +1,47 @@
+package com.manisha.java.oops.Inheritance;
+
+public class Strings {
+    public static void main(String[]args){
+        printoinfo("Hello World");
+        printoinfo("");
+        printoinfo("\t \n");
+
+        String hello = "Hello World";
+        System.out.printf("index of r : %d %n", hello.indexOf('r'));
+
+        System.out.printf("index of l: %d %n", hello.indexOf('l'));
+        System.out.printf("last index of l: %d %n", hello.lastIndexOf('l'));
+        System.out.printf("index of l: %d %n", hello.indexOf('l', 3));
+        System.out.printf("last index of l: %d %n", hello.lastIndexOf('l',8));
+
+        String helloWorldLower = hello.toLowerCase();
+        if(hello.equals(helloWorldLower)){
+            System.out.println("Value match!");
+        }
+        if(hello.equalsIgnoreCase(helloWorldLower)){
+            System.out.println("Value match Ignoring Case! ");
+        }
+        if(hello.startsWith("Hello")){
+            System.out.println("String starts with hello. ");
+        }
+        if(hello.endsWith("World")){
+            System.out.println("String ends with world. ");
+        }
+        if(hello.contains("World")){
+            System.out.println("String contains world. ");
+        }
+
+    }
+    public static void printoinfo(String string){
+        int length = string.length();
+        System.out.printf("Length : %d %n", length);
+
+        if(string.isEmpty()){
+            System.out.printf("String is empty" );
+            return;
+        }
+        System.out.printf("First char : %c %n", string.charAt(0));
+        System.out.printf("Last char : %c %n", string.charAt(length- 1));
+
+    }
+}
--- src/main/java/com/manisha/java/oops/Inheritance/TextBlock.java
+++ src/main/java/com/manisha/java/oops/Inheritance/TextBlock.java
@@ -5,26 +5,32 @@ import com.manisha.java.util.Print;
 //Text block
 //Format specifiers- %d(decimal int value),
 public class TextBlock {
-
     public static void main (String[]args){
 
         String bulletIt = "Print in bullet list : \n" +
                 "\t\u2022 First \n" +
                 "\t \u2022 Second \n";
         Print.print(bulletIt);
-
-
+        //------------------------------------------------
         String textBlock = """
                 Print a Bulleted list:
                         \u2022 First 
                               \u2022 Second """;
         Print.print(textBlock);
-
+        //------------------------------------------------
         int age = 35;
-        Print.print("Your age is %d%n" , age);
+        String name = "Rohan";
         int yearOfBirth = 2023 - age;
-        Print.print("Age = %d, Birth year = %d", age, yearOfBirth);
-        Print.print("Your age is %fs%n", (float) age);
+        //printf- format
+        System.out.printf("Name : %s , Age : %d  Birth year : %d " , name, age, yearOfBirth);
+        System.out.printf("Your age is %.2f%n ", (float) age);
+
+        int age2 = 20;
+        float height = 5.8f;
+        String name2 = "John";
+
+        System.out.printf("Name: %s , Age: %d , Height: %.2f%n ", name2, age2, height);
+
 
     }
 }
