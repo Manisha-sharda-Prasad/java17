ade74901cca6a3c3a0a36ec517f95356468f9c36
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-05-24 17:01:35 -0700

    pass by Reference

--- src/main/java/com/manisha/java/oops/Polymorphism/CallByReference/CallByReference.java
+++ src/main/java/com/manisha/java/oops/Polymorphism/CallByReference/CallByReference.java
@@ -1,17 +1,18 @@
-package com.manisha.java.oops.Polymorphism;
-
-
-
+package com.manisha.java.oops.Polymorphism.CallByReference;
 public class CallByReference {
 
     public static void modifyReference(StringBuilder builder) {
         builder.append(" World!"); // Changes to builder inside this method affect sb
+
     }
+    //-------------Main----------------------
     public static void main(String[] args) {
+        //1st e.g--
         StringBuilder sb = new StringBuilder("Hello");
         System.out.println("Before: " + sb);
         modifyReference(sb);
         System.out.println("After: " + sb);
+
     }
 }
 /*
---
+++ src/main/java/com/manisha/java/oops/Polymorphism/CallByReference/Reference.java
@@ -0,0 +1,20 @@
+package com.manisha.java.oops.Polymorphism.CallByReference;
+
+public class Reference {
+    int value;
+    public static void modifyObject(Reference obj) {
+        obj.value = 20; // Changes to 'obj' affect the original object
+    }
+    //-------------main-------------------
+    public static void main(String[] args) {
+
+        Reference obj = new Reference();
+        obj.value = 10;
+
+        modifyObject(obj);//(obj) passed by reference
+        System.out.println("After method call: " + obj.value); // Output: 20
+    }
+}
+
+//the "value" of "obj.value" is changed to '20'-- because obj is "passed by reference",
+// and the reference allows the method to modify the -"original object"-.
