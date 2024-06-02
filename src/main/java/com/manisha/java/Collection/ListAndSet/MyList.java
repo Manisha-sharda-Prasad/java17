1c52e8c21efff06944cac568f7f16283ada72df9
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-06-02 16:25:04 -0700

    array list <> .set(), .replace()

--- src/main/java/com/manisha/java/Collection/ListAndSet/MyList.java
+++ src/main/java/com/manisha/java/Collection/ListAndSet/MyList.java
@@ -21,17 +21,19 @@ public class MyList {
         groceryItems[2] = new GroceryItem("oranges","PRODUCE",5);
         Print.print(Arrays.toString(groceryItems)); //toString()--overriding this method  above
 
-
         //<Type Argument> = <> --diamond-Operator---------------
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
         groceryList.add(new GroceryItem("Milk","DAIRY",2));
         groceryList.add(new GroceryItem("Oranges","PRODUCE",7));
-        groceryList.add(0,
+        groceryList.set(0,
                 new GroceryItem("Apples","PRODUCE",8));
+        groceryList.remove(1);
         Print.print(groceryList);
 
     }
 }
 //toString method--- Array class also contains "toString method"- nicely formats an array of objects.
 //The String.format--- method signature,This template contains ‘%’ chars to represent where the various Objects should be placed within it.
+//set()replace the first item, puts itself.
+//remove()
