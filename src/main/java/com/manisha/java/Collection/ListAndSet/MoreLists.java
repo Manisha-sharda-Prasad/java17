80c9e58b843b035a1a9c4cd135e97148001376dc
Manisha-sharda-Prasad <prasad4manisha@gmail.com>
2024-06-02 18:15:42 -0700

    array list <>  //Methods-java.util--  //.contains()  //.get(),  //.add(), .remove(),  //.addAll(), .removeAll(),  //.retainAll(),  //.clear(),  //.isEmpty(),  //.indexOf(),  //.LastIndexOf(),  //.List.of(),  //.Arrays.asList(),  //.sort(),  //.sort(Comparator.naturalOrder(),  //.sort(Comparator.reverseOrder(),  //.size()  //---

--- src/main/java/com/manisha/java/Collection/ListAndSet/MoreLists.java
+++ src/main/java/com/manisha/java/Collection/ListAndSet/MoreLists.java
@@ -3,6 +3,8 @@ package com.manisha.java.Collection.ListAndSet;
 import com.manisha.java.util.Print;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreLists {
@@ -23,10 +25,63 @@ public class MoreLists {
         //better ways to add elements----
         //passing list of Strings, to static method on List -(List.of)---
         ArrayList<String> nextList = new ArrayList<>(
-                List.of("Pickles", "mustard","cheese"));
+                List.of("pickles", "mustard","cheese","papaya","naan"));
         Print.print(nextList);
-        //add.all(other var) array-----
+        //addAll(other var) array-----
         groceries.addAll(nextList);
         Print.print(groceries);
+
+        //other in-built methods used for elements of array ------
+        Print.print("third item = " + groceries.get(2));
+
+        if(groceries.contains("mustard")){Print.print("contains mustard!");}
+
+        groceries.add("yogurt");
+
+        Print.print("first = "+ groceries.indexOf("yogurt"));
+        Print.print("last = "+ groceries.lastIndexOf("yogurt"));
+
+        groceries.remove(1);
+        groceries.remove("yogurt");
+        Print.print(groceries);
+
+        groceries.removeAll(List.of("bananas","eggs"));
+        Print.print(groceries);
+
+        groceries.retainAll(List.of("apples","milk","mustard", "naan", "cheese"));
+        Print.print(groceries);
+
+        groceries.clear();
+        Print.print(groceries);
+        Print.print("isEmpty = "+ groceries.isEmpty());
+
+        groceries.addAll(Arrays.asList("eggs", "watermelon", "apricot", "ham", "mints", "cucumber"));
+        Print.print(groceries);
+
+        groceries.sort(Comparator.naturalOrder());
+        Print.print(groceries);
+
+        groceries.sort(Comparator.reverseOrder());
+        Print.print(groceries);
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        Print.print(Arrays.toString(groceryArray));
+
     }
 }
+//Methods-java.util--
+//.contains()
+//.get(),
+//.add(), .remove(),
+//.addAll(), .removeAll(),
+//.retainAll(),
+//.clear(),
+//.isEmpty(),
+//.indexOf(),
+//.LastIndexOf(),
+//.List.of(),
+//.Arrays.asList(),
+//.sort(),
+//.sort(Comparator.naturalOrder(),
+//.sort(Comparator.reverseOrder(),
+//.size()
