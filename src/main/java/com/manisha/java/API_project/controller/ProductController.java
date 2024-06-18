c6ecc53342c0e49defc139bf3e2135e2e58497fe
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-06-18 00:35:50 -0700

    API-Project controller --> service --> repo

--- src/main/java/com/manisha/java/API_project/controller/ProductController.java
+++ src/main/java/com/manisha/java/API_project/controller/ProductController.java
@@ -11,6 +11,7 @@ import java.util.Comparator;
 import java.util.List;
 
 @RestController
+//@RequestMapping("product")
 // Servlet - http request / http response-body-json
 public class ProductController
 {
