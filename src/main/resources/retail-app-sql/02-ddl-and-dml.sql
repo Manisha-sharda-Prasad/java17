e3790969df93724d02ff51d10bbcc2cbb3dce220
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-06-21 15:02:49 -0700

    SQL postgres update

--- src/main/resources/retail-app-sql/02-ddl-and-dml.sql
+++ src/main/resources/retail-app-sql/02-ddl-and-dml.sql
@@ -1,3 +1,117 @@
+-- Customers Table
+CREATE TABLE IF NOT EXISTS  retailapp.Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(255) NOT NULL,
+    ContactName VARCHAR(255),
+    Country VARCHAR(255),
+    Address VARCHAR(255),
+    City VARCHAR(255),
+    PostalCode VARCHAR(20)
+);
+
+-- Categories Table
+CREATE TABLE IF NOT EXISTS  retailapp.Categories (
+    CategoryID INT PRIMARY KEY,
+    CategoryName VARCHAR(255) NOT NULL,
+    Description TEXT
+);
+
+-- Employees Table
+CREATE TABLE IF NOT EXISTS  retailapp.Employees (
+    EmployeeID INT PRIMARY KEY,
+    LastName VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    BirthDate DATE,
+    Photo VARCHAR(255),
+    Notes TEXT
+);
+
+-- Suppliers Table
+CREATE TABLE IF NOT EXISTS  retailapp.Suppliers (
+    SupplierID INT PRIMARY KEY,
+    SupplierName VARCHAR(255) NOT NULL,
+    ContactName VARCHAR(255),
+    Country VARCHAR(255),
+    Address VARCHAR(255),
+    City VARCHAR(255),
+    PostalCode VARCHAR(20)
+);
+
+-- Shippers Table
+CREATE TABLE IF NOT EXISTS  retailapp.Shippers (
+    ShipperID INT PRIMARY KEY,
+    ShipperName VARCHAR(255) NOT NULL,
+    Phone VARCHAR(255)
+);
+
+-- Products Table
+CREATE TABLE IF NOT EXISTS  retailapp.Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(255) NOT NULL,
+    SupplierID INT,
+    CategoryID INT,
+    Unit VARCHAR(255),
+    Price DECIMAL(10, 2),
+    FOREIGN KEY (SupplierID) REFERENCES retailapp.Suppliers(SupplierID),
+    FOREIGN KEY (CategoryID) REFERENCES retailapp.Categories(CategoryID)
+);
+
+-- Orders Table
+CREATE TABLE IF NOT EXISTS  retailapp. Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    EmployeeID INT,
+    OrderDate DATE,
+    ShipperID INT,
+    FOREIGN KEY (CustomerID) REFERENCES retailapp.Customers(CustomerID),
+    FOREIGN KEY (EmployeeID) REFERENCES retailapp.Employees(EmployeeID),
+    FOREIGN KEY (ShipperID) REFERENCES retailapp.Shippers(ShipperID)
+);
+
+-- OrderDetails Table
+CREATE TABLE IF NOT EXISTS  retailapp. OrderDetails (
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES retailapp.Orders(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES retailapp.Products(ProductID)
+);    
+
+-- SHOP Table
+CREATE TABLE IF NOT EXISTS  retailapp. Shop (
+    ShopID INT PRIMARY KEY,
+    ShopName VARCHAR(255) NOT NULL,
+    Address VARCHAR(255),
+    City VARCHAR(255),
+    PostalCode VARCHAR(20),
+    Country VARCHAR(255)
+);
+
+-- CART Table
+CREATE TABLE IF NOT EXISTS  retailapp. Cart (
+    CartID INT PRIMARY KEY,
+    CustomerID INT,
+    ProductID INT,
+    Quantity INT,
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (CustomerID) REFERENCES retailapp.Customers(CustomerID),
+    FOREIGN KEY (ProductID) REFERENCES retailapp.Products(ProductID)
+);
+
+
+
+-- DROP TABLE IF NOT EXISTS  retailapp. CATEGORIES ;
+-- DROP TABLE IF NOT EXISTS  retailapp. CUSTOMERS ;
+-- DROP TABLE IF NOT EXISTS  retailapp. EMPLOYEES ;
+-- DROP TABLE IF NOT EXISTS  retailapp. ORDERDETAILS ;
+-- DROP TABLE IF NOT EXISTS  retailapp. ORDERS ;
+-- DROP TABLE IF NOT EXISTS  retailapp. PRODUCTS ;
+-- DROP TABLE IF NOT EXISTS  retailapp.  SHIPPERS ;
+-- DROP TABLE IF NOT EXISTS  retailapp. SUPPLIERS ;
+
+
+
 
 -- Inserting data into Customers table
 INSERT INTO Customers (CustomerID, CustomerName, ContactName, Country, Address, City, PostalCode) VALUES
@@ -139,3 +253,5 @@ INSERT INTO Cart (CartID, CustomerID, ProductID, Quantity) VALUES
 (13, 3, 13, 3),
 (14, 4, 14, 1),
 (15, 5, 15, 2);
+
+commit;
