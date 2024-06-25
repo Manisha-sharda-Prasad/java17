e1a611887b6e5b576f1466d31757a47718edf223
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-06-24 23:08:28 -0700

    Streams: Intermediate Operations, terminal operation

--- src/main/java/com/manisha/java/Streams/StreamMethods.java
+++ src/main/java/com/manisha/java/Streams/StreamMethods.java
@@ -11,14 +11,24 @@ public class StreamMethods {
 
         // Intermediate Operations: --
         List<Integer> processesNum = num.stream()
-                .filter( n -> n % 3 == 0 ) //3,9,6,12..
-                .map( n -> n + n)          //6,12,18,24
-                .sorted()
-                .distinct()
-                .collect(Collectors.toList()); //collect results
+                                    .filter( n -> n % 3 == 0 ) //3,9,6,12..
+                                    .map( n -> n + n)          //6,12,18,24
+                                    .sorted()
+                                    .distinct()
+                                    .collect(Collectors.toList()); //collect results
 
         // Terminal Operations: --
         processesNum.forEach(System.out::println);
+
+
+        // list of String --
+        List<String> characters = Arrays.asList("phone", "tablet", "computer", "laptop", "mouse" , "pen");
+                characters.stream()
+                        //.skip(characters.size()-1)                            //long
+                        .limit(characters.size()-1)                     // long
+                        .map ((item) -> { return item.toUpperCase()+"==="; })   //fn
+                        .sorted((item1 ,item2) -> {return 0;} )                 //comparator
+                        .forEach((item) -> { System.out.println(item);});       // consumer
     }
 }
 // Streams-- Process sequence of elements(like collection) in a functional style - readable/ concise /parallel process.
@@ -26,7 +36,13 @@ public class StreamMethods {
 //1.Source: Obtain stream from a data source. (stream)
 
 //2.Intermediate Operations:  Chain one or more intermediate operations to transform the stream.
-//(Filter, map, sorted, distinct)
+    //(Filter, map, sorted, distinct)
 
 //3.Terminal Operation:  Apply a terminal operation to produce a result or a side -effect.
-//(collect, forEach, reduce, count)
+    //(collect, forEach, reduce, count).
+
+//4. Functional Interfaces--
+    //1.Predicates-- takes value of 1 arg, returns boolean.
+    //2.Functions-- takes 1 arg , returns 1 result.
+    //3.Consumers-- takes 1 arg , returns nothing
+    //4.Suppliers-- takes no arg, returns result
