58a2ffd315221cd7bb62c1608370476ba223b922
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-07-10 13:01:47 -0700

    rest api

--- src/main/java/com/manisha/java/retailapp_project/entity/Shop.java
+++ src/main/java/com/manisha/java/retailapp_project/entity/Shop.java
@@ -1,4 +1,37 @@
 package com.manisha.java.retailapp_project.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "shop", schema = "retailapp")
 public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String country;
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
