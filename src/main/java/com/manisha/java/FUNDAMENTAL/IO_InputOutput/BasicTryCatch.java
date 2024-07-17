e6d266368eb118b00f7919556b8b4405ae389eca
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-07-17 14:17:02 -0700

    Exception Handling

--- src/main/java/com/manisha/java/FUNDAMENTAL/IO_InputOutput/BasicTryCatch.java
+++ src/main/java/com/manisha/java/FUNDAMENTAL/IO_InputOutput/BasicTryCatch.java
@@ -4,23 +4,26 @@ public class BasicTryCatch {
     public static void main(String[] args) {
         tryCatch();
         tryCatchFinally();
+        multipleCatch();
 
     }
 
-    public static void tryCatch(){
-            try {
-                int result = 20/ 0;
-            } catch (ArithmeticException e) {
-                System.out.println("Arithmetic Exception caught ");
-            }
-            System.out.println("Still Continuing Program");
-    }
-
-    public static void tryCatchFinally(){
+    //Arithmetic Exception--
+    public static void tryCatch() {
         try {
-            int[] result = {1,2,3,4};
+            int result = 20 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception caught ");
+        }
+        System.out.println("Still Continuing Program");
+    }
+
+    // ArrayIndexOutOfBoundsException--
+    public static void tryCatchFinally() {
+        try {
+            int[] result = {1, 2, 3, 4};
             System.out.println(result[6]);
-        } catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Array Index Exception Caught.");
         } finally {
             System.out.println("This will Always Run.");
@@ -28,5 +31,22 @@ public class BasicTryCatch {
         System.out.println("Still Continuing Program");
 
     }
+    // NullPointerException--
+    public static void multipleCatch() {
+        try {
+            String str = null;
+            System.out.println(str.length());
+
+            int result = 12 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception caught : not divisible by 0");
+        } catch (NullPointerException e) {
+            System.out.println("Null Pointer Exception caught ");
+        } finally {
+            System.out.println("This will Always Run.");
+        }
+
+    }
+
 
 }
