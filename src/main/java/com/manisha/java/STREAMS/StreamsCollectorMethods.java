53fddcac70566c08b115cc1533e93920184f260f
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-07-19 12:51:21 -0700

    java Streams Methods- Collectors

--- src/main/java/com/manisha/java/STREAMS/StreamsCollectorMethods.java
+++ src/main/java/com/manisha/java/STREAMS/StreamsCollectorMethods.java
@@ -4,36 +4,51 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 @Slf4j
 public class StreamsCollectorMethods {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8);
-        List <String> chars = Arrays.asList("Apple", "Banana", "Orange");
+        List <String> chars = Arrays.asList("Apple", "Banana", "Orange", "apricot", "cherry");
 
         //counting--
         Long count = numbers.stream()
-                           .collect(Collectors.counting());
-                 System.out.println(count);
-
+                            .collect(Collectors.counting());
+                      System.out.println(count);
 
         //summingInt--
         int sum = numbers.stream()
-                .collect(Collectors.summingInt(n -> n));
-        System.out.println(sum);
+                         .collect(Collectors.summingInt(n -> n));
+                   System.out.println(sum);
 
         //averagingInt--
         double average = numbers.stream()
-                .collect(Collectors.averagingInt(n -> n));
-        System.out.println(average);
+                                .collect(Collectors.averagingInt(n -> n));
+                          System.out.println(average);
 
-        //joining--
+        //joining (CharSequence delimiter)--
         String string = chars.stream()
-                .collect(Collectors.joining("::"));
-        System.out.println(string);
+                             .collect(Collectors.joining("::"));
+                       System.out.println(string);
 
+        //toList--
+        List list = numbers.stream()
+                           .collect(Collectors.toList());
+                     System.out.println(list);
 
+        //toSet--
+        Set set = chars.stream()
+                       .collect(Collectors.toSet());
+                 System.out.println(set);
+
+        //toMap (keyMapper, valueMapper)--
+        Map <String,Integer> charMapLength = chars.stream()
+                                                  .collect(Collectors.toMap(m->m, m->m.length()));
+                                            System.out.println(charMapLength);
+        //groupingBy(classifier)--
 
 
 
